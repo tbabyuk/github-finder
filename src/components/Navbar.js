03d51f848ff3e1c4d6e44800e175c6548e9b5622
Terry Babyuk <tbabyuk@gmail.com>
2023-02-09 23:56:15 -0500

    initial commit, basic header completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+// import styles
+import "./Navbar.css"
+
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="navbar-logo">Github Finder</div>
+        <ul className="navbar-menu">
+            <li>Link 1</li>
+            <li>Link 1</li>
+            <li>Link 1</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
